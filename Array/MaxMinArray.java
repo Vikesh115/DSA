0405d96c8dd1cefda2ad267d319dfdda407b5a3f
VIKESH RAUT <85770809+Vikesh115@users.noreply.github.com>
2022-07-20 14:42:08 +0530

    Update MaxMinArray.java

--- Array/MaxMinArray.java
+++ Array/MaxMinArray.java
@@ -2,35 +2,33 @@ package Array;
 
 import java.util.Scanner;
 
-public class MaxMinArray {
-    
-static int Max(int[] arr, int n)
-   {
-    int i;
-     int max = arr[0];
-
-    for(i=0; i<n; i++)
-    {
-        if(arr[i]>max);
-        {
-            max = arr[i];
-        }
-    }
-    return max;
-   }
-
-
-
+public class MaxMinArray
+{
    public static void main(String[] args)
     {
+         int n,i,maxVal,minVal;
+
          Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
-        int arr[] = new int[n];
-         for(int i = 0; i<n; i++)
+          n = sc.nextInt();
+
+         int arr[] = new int[n];
+         for( i = 0; i<n; i++)
          {
            arr[i] = sc.nextInt();
          }
-      System.out.println("Max is : " + Max(arr, n));
+
+         maxVal = minVal = arr[0];
+    
+        for( i=1; i<n; i++){
+            if(maxVal < arr[i]){
+            maxVal = arr[i];
+            }
+            else if(minVal > arr[i]){
+                minVal = arr[i];
+            }
+        }
+        System.out.println("Min is : " + minVal);
+        System.out.println("Max is : " + maxVal);
     }
 
 }
