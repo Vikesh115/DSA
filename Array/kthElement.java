b902210fcceab807d8ad4ef7f2a6410f3515f988
VIKESH RAUT <85770809+Vikesh115@users.noreply.github.com>
2022-07-21 12:35:04 +0530

    Enter

---
+++ Array/kthElement.java
@@ -0,0 +1,28 @@
+package Array;
+import java.util.Arrays;
+import java.util.Scanner;
+public class kthElement{
+
+    
+public static int Kthelement(int[] arr, int k)
+{
+
+    Arrays.sort(arr);
+    return arr[k-1];
+}
+
+
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = sc.nextInt();
+    }
+
+    System.out.println("K'th element is : " +Kthelement(arr,k));
+}
+}
