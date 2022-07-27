14f0b221c0e6a54dc84fe6b4a19a1dfe4bb82843
VIKESH RAUT <85770809+Vikesh115@users.noreply.github.com>
2022-07-27 22:10:34 +0530

    Update FindElement.java

--- Array/FindElement.java
+++ Array/FindElement.java
@@ -1,9 +1,32 @@
 package Array;
 
+import java.util.Scanner;
+
 public class FindElement {
     
+    static void findElement(int arr[], int n)
+    {
+        for(int i = 0; i<n; i++)
+        {
+            int k = arr[2];
+            if(arr[i] == k)
+            {
+                System.out.println("Found");
+            }
+        }
+    }
 
 
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
 
-    
+        findElement(arr, n);
+    }
 }
